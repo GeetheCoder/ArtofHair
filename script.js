db9f91575abfbc13438c3234df44a8b00ed88de0
Gee <42432144+grmays144@users.noreply.github.com>
2019-05-13 21:11:35 -0500

    Intialize
    Intialize

---
+++ script.js
@@ -0,0 +1,39 @@
+
+
+ var imageResize = () => {
+
+  
+
+    
+     document.getElementById("blowUPic").style.fontSize = "70px";
+     
+
+  } 
+    
+   
+
+  var mouseOut = () => {
+
+    document.getElementById("blowUPic").style.fontSize= "50px";
+ 
+
+
+    
+
+
+  }
+  
+ 
+  
+
+      
+
+ 
+
+
+function alert2(par){
+  console.log("nooo");
+
+}
+
+
